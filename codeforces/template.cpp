b58a614557b719e54a6c8f4d20c239fd83fd7b30
omar khalil <o.mar.om.123m@gmail.com>
2024-05-24 23:57:22 +0300

    add template

---
+++ codeforces/template.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include <map>
+using namespace std;
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define _USE_MATH_DEFInES
+#define OP                            \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+/*ll u = 1e9;
+int Lcm(vector<int> arr, int idx)
+{
+  // lcm(a,b) = (a*b/gcd(a,b))
+  if (idx == arr.size() - 1)
+  {
+    return arr[idx];
+  }
+  int a = arr[idx];
+  int b = Lcm(arr, idx + 1);
+  return (a * b / __gcd(a, b)); // __gcd(a,b) is inbuilt library function
+}*/
+int main() {
+    OP;
+    //   solution goes brrr
+    return 0;
+}
